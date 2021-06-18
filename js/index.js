3ee4d9974ec4dedb3f0db4337fbb559765477554
Sanyam Jain <sanyamjaino98@gmail.com>
2021-06-19 01:40:17 +0530

    Added debouncing in search bar

--- js/index.js
+++ js/index.js
@@ -1,24 +1,41 @@
 const search = document.getElementById('search')
 const matchList = document.getElementById('match-list')
 
-search.addEventListener('input', () => searchStates(search.value))
+const debounce = (fn, d) => {
+    let timer
+    let context = this
+
+    // cant access search.value here
+
+    return () => {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn.apply(context, [search.value]) //context is nothing but the window object here
+        }, d)
+    }
+}
+
+const debounceRequest = debounce(filterSearch, 500)
 
 // search states from json data and filter
-async function searchStates(searchText) {
+async function filterSearch(searchText) {
 
-    const res = await fetch('./data/data.json')
-    const states = await res.json()
-
-    const regex = new RegExp(`^${searchText}`, 'gi') // gi flag removes the case sensitiveness
-
-    // get matches to current text input
-    let matches = states.filter(state => {
-        return state.name.match(regex) || state.abbr.match(regex)
-    })
+    let matches
 
     // do not show any result if input string in empty
     if (searchText.trim().length === 0) {
         matches = []
+    } else {
+        console.log('making api call')
+        const res = await fetch('./data/data.json')
+        const states = await res.json()
+
+        const regex = new RegExp(`^${searchText}`, 'gi') // gi flag removes the case sensitiveness
+
+        // get matches to current text input
+        matches = states.filter(state => {
+            return state.name.match(regex) || state.abbr.match(regex)
+        })
     }
 
     // show matches with list
@@ -39,3 +56,5 @@ function outputHtml(matches) {
         matchList.innerHTML = ''
     }
 }
+
+search.addEventListener('keyup', () => debounceRequest())
